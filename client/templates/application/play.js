7dcd6a28597b03f24459ffa1890a9ed2dcb613bf
Ben Rothman <bensrothman@gmail.com>
2015-12-07 17:25:32 -0600

    bug fixes

--- client/templates/application/play.js
+++ client/templates/application/play.js
@@ -1,5 +1,13 @@
 Template.play.helpers({
-  match: () => RPSLS.matchInfo(RPSLS.Collections.Rounds.find({}, { sort: { played: -1 } }).fetch())
+  match: () => {
+    let user = Meteor.user()
+
+    if (user) {
+      let rounds = RPSLS.Collections.Rounds.find({ players: user.username }, { sort: { played: -1 } }).fetch()
+
+      return RPSLS.matchInfo(rounds)
+    }
+  }
 })
 
 Template.play.events({
@@ -8,6 +16,10 @@ Template.play.events({
   }
 })
 
+let debounceRedirect = _.debounce(function (matchId) {
+  FlowRouter.go('matchPage', { matchId })
+}, 500)
+
 Template.play.onCreated(function () {
   if (Meteor.user()) {
     this.subscribe('myRounds', Meteor.user().username, {
@@ -15,14 +27,15 @@ Template.play.onCreated(function () {
         if (RPSLS.Collections.Rounds.find().count() === 0) {
           FlowRouter.go('landing')
         }
+
+        RPSLS.Collections.Rounds.find({ players: Meteor.user().username }).observe({
+          removed: (round) => {
+            // match ended. redirect to play page
+            console.log(round)
+            debounceRedirect(round.matchId)
+          }
+        })
       }
     })
   }
-
-  RPSLS.Collections.Rounds.find().observe({
-    removed: (round) => {
-      // match ended. redirect to play page
-      FlowRouter.go('matchPage', { matchId: round.matchId })
-    }
-  })
 })
